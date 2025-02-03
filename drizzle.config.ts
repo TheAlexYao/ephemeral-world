74de3fa545485ca5754e1a33df3613fa3ff4bd09
Alexander Yao <9095664+TheAlexYao@users.noreply.github.com>
2025-02-03 19:42:35 +0700

    drizzle next config errors

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,7 @@ export default {
   schema: "./src/drizzle/schema.ts",
   out: "./src/drizzle/migrations",
   dialect: "sqlite",
-  driver: "libsql",
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 } satisfies Config; 
